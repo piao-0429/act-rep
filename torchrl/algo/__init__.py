0cdd153d149620b8ddbd2427861d9ef5fac31341
piao-0429 <huapu0429@gmail.com>
2022-03-16 11:26:08 +0000

    init

---
+++ torchrl/algo/__init__.py
@@ -0,0 +1,14 @@
+from .off_policy import *
+
+__all__ = [
+    'SAC',
+    'MTSAC',
+    'MTMHSAC',
+    'DDPG',
+    'TwinSAC',
+    'TwinSACQ',
+    'TD3',
+    'ARSAC',
+    'ARSAC_v2'
+    'ARPPO'
+]
---
+++ torchrl/algo/off_policy/__init__.py
@@ -0,0 +1,8 @@
+from .sac import SAC
+from .mt_sac import MTSAC
+from .mtmh_sac import MTMHSAC
+from .twin_sac import TwinSAC
+from .twin_sac_q import TwinSACQ
+
+from .ar_sac import ARSAC
+from .ar_sac_v2 import ARSAC_v2
